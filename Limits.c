70b175cdb245de0633755d69e8ad02c721a74b48
Irrenhauspatient <andreasscheuer85@gmx.de>
2021-07-05 05:00:12 +0200

    limits bearbeitet

--- Limits.c
+++ Limits.c
@@ -23,51 +23,23 @@ int main()
     printf("Min: unsigned int =  %s\n", "nicht definiert");
     printf("Max: unsigned int =  %u\n", UINT_MAX);
 
-    scanf("%d", &digitInteger);
+    printf("\nWerte der Bibliotheke:");
 
-    /*
-    define CHAR_BIT      8
-#define SCHAR_MIN   (-128)
-#define SCHAR_MAX     127
-#define UCHAR_MAX     0xff
+    printf("#define SCHAR_MIN   (-128)\n");
+    printf("#define SCHAR_MAX     127\n");
 
-#ifndef _CHAR_UNSIGNED
-    #define CHAR_MIN    SCHAR_MIN
-    #define CHAR_MAX    SCHAR_MAX
-#else
-    #define CHAR_MIN    0
-    #define CHAR_MAX    UCHAR_MAX
-#endif
+    printf("#define SHRT_MIN    (-32768)\n");
+    printf("#define SHRT_MAX      32767\n");
 
-#define MB_LEN_MAX    5
-#define SHRT_MIN    (-32768)
-#define SHRT_MAX      32767
-#define USHRT_MAX     0xffff
-#define INT_MIN     (-2147483647 - 1)
-#define INT_MAX       2147483647
-#define UINT_MAX      0xffffffff
-#define LONG_MIN    (-2147483647L - 1)
-#define LONG_MAX      2147483647L
-#define ULONG_MAX     0xffffffffUL
-#define LLONG_MAX     9223372036854775807i64
-#define LLONG_MIN   (-9223372036854775807i64 - 1)
-#define ULLONG_MAX    0xffffffffffffffffui64
+    printf("#define INT_MIN     (-2147483647 - 1) \n");
+    printf("#define INT_MAX       2147483647\n");
 
-#define _I8_MIN     (-127i8 - 1)
-#define _I8_MAX       127i8
-#define _UI8_MAX      0xffui8
+    printf("\n");
+    printf("#define UCHAR_MAX     0xff\n");
 
-#define _I16_MIN    (-32767i16 - 1)
-#define _I16_MAX      32767i16
-#define _UI16_MAX     0xffffui16
+    printf("\n");
+    printf("#define USHRT_MAX     0xffff\n");
 
-#define _I32_MIN    (-2147483647i32 - 1)
-#define _I32_MAX      2147483647i32
-#define _UI32_MAX     0xffffffffui32
-
-#define _I64_MIN    (-9223372036854775807i64 - 1)
-#define _I64_MAX      9223372036854775807i64
-#define _UI64_MAX     0xffffffffffffffffui64
-
-    */
+    printf("\n");
+    printf("#define UINT_MAX      0xffffffff\n");
 }
